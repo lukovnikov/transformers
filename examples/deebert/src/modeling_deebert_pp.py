015055eedc75f95dfabcf448f8511395dd413ef4
lukovnikov <lukovnikov@users.noreply.github.com>
2020-10-08 14:49:49 +0200

    baselines

--- examples/deebert/src/modeling_deebert_pp.py
+++ examples/deebert/src/modeling_deebert_pp.py
@@ -95,7 +95,8 @@ class DeeBertEncoder(nn.Module):
                 ee = self.early_exits[i]
                 if self.mode == "baseline":
                     ee = self.early_exits[-1]
-                exit_logit = ee(tuple([co.detach() for co in current_outputs]))[0]
+                # exit_logit = ee(tuple([co.detach() for co in current_outputs]))[0]
+                exit_logit = ee(current_outputs)[0]
                 # early_exit = ee(tuple([torch.zeros_like(co) for co in current_outputs]))
                 exit_logit = torch.zeros_like(exit_logit)
                 exit_logit[:, 0] = 100
