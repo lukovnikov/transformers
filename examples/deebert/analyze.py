267f67eafbef6eb5661efe9b03677e6b73c63daf
lukovnikov <lukovnikov@users.noreply.github.com>
2020-10-08 20:32:32 +0200

    analyzer

--- examples/deebert/analyze.py
+++ examples/deebert/analyze.py
@@ -7,9 +7,9 @@ def main(
         p="",
     ):
     d = np.load(p)
-    numex, numlayer = d["time"].shape
+    numex, numlayer = d["times"].shape
     time_argsorted = np.argsort(d["times"].reshape(-1)).reshape(numex, numlayer)
-    time_sorted = d["times"].reshape(-1)[time_argsorted].reshape(numex, numlayer)
+        time_sorted = d["times"].reshape(-1)[time_argsorted].reshape(numex, numlayer)
     min_time = d["times"].min(-1).max()
     max_time = d["times"].max()
 
