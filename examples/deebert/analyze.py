1f40330bb6436387235dc840dc02c728016fc352
lukovnikov <lukovnikov@users.noreply.github.com>
2020-10-08 20:30:03 +0200

    analyzer

---
+++ examples/deebert/analyze.py
@@ -0,0 +1,20 @@
+import numpy as np
+import fire
+from IPython import embed
+
+
+def main(
+        p="",
+    ):
+    d = np.load(p)
+    time_argsorted = np.argsort(d["times"].reshape(-1))
+    time_sorted = d["time"].reshape(-1)[time_argsorted]
+    min_time = d["times"].min(-1).max()
+    max_time = d["times"].max()
+
+    embed()
+
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
